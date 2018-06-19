88e7227b06f91546dbb86a23c140ee18ef83a4a9
notebook\grig <grig48@gmail.com>
2018-06-19 17:26:00 +0300

    HomeworkKravchenko

--- lecture-5-HomeWork-Kravchenko/src/test/java/myprojects/automation/assignment5/utils/DriverFactory.java
+++ lecture-5-HomeWork-Kravchenko/src/test/java/myprojects/automation/assignment5/utils/DriverFactory.java
@@ -23,52 +23,6 @@ public class DriverFactory {
      * @return New instance of {@link WebDriver} object.
      */
     public static WebDriver initDriver(String browser) {
-//            switch (browser) {
-//            case "firefox":
-//                System.setProperty(
-//                        "webdriver.gecko.driver",
-//                        new File(DriverFactory.class.getResource("/geckodriver.exe").getFile()).getPath());
-//                return new FirefoxDriver();
-//            case "ie":
-//            case "internet explorer":
-//                System.setProperty(
-//                        "webdriver.ie.driver",
-//                        new File(DriverFactory.class.getResource("/IEDriverServer.exe").getFile()).getPath());
-//                InternetExplorerOptions ieOptions = new InternetExplorerOptions()
-//                        .destructivelyEnsureCleanSession();
-//                ieOptions.setCapability(InternetExplorerDriver.NATIVE_EVENTS, false);
-//                return new InternetExplorerDriver(ieOptions);
-//            case "headless-chrome":
-//                System.setProperty(
-//                        "webdriver.chrome.driver",
-//                        new File(DriverFactory.class.getResource("/chromedriver.exe").getFile()).getPath());
-//                ChromeOptions options = new ChromeOptions();
-//                options.addArguments("headless");
-//                options.addArguments("window-size=800x600");
-//                return new ChromeDriver();
-//            case "remote-chrome":
-//                ChromeOptions optionsRemote = new ChromeOptions();
-//                try {
-//                    return new RemoteWebDriver(new URL("http:localhost:4444/wd/hub"), optionsRemote);
-//                } catch (MalformedURLException e) {
-//                    e.printStackTrace();
-//                }
-//                return null;
-//            case "mobile":
-//                System.setProperty(
-//                        "webdriver.chrome.driver",
-//                        new File(DriverFactory.class.getResource("/chromedriver.exe").getFile()).getPath());
-//                Map<String, String> mobileEmulation = new HashMap<>();
-//                mobileEmulation.put("deviceName", "iPhone 6");
-//                ChromeOptions chromeOptions = new ChromeOptions();
-//                chromeOptions.setExperimentalOption("mobileEmulation", mobileEmulation);
-//                return new ChromeDriver(chromeOptions);
-//            case "chrome":
-//            default:
-//                System.setProperty(
-//                        "webdriver.chrome.driver",
-//                        new File(DriverFactory.class.getResource("/chromedriver.exe").getFile()).getPath());
-//                return new ChromeDriver();
 
         switch (browser) {
             case "firefox":
